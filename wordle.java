51c340cf45da3d231a0b92b85de6fec5cf05097a
Aditi Bhaskar <44757648+aditi-bhaskar@users.noreply.github.com>
2022-04-29 11:22:37 -0700

    Add files via upload

---
+++ wordle.java
@@ -0,0 +1,15 @@
+
+
+import java.util.* ;
+class Main {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in) ;
+    boolean is_game_over = false ;
+
+    System.out.println("playing wordle") ; 
+
+    
+  
+  }
+}
