7aed0d1ab08d603cd74ac2e9659959940fcc3e6f
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-05-01 14:02:37 -0700

    added random length mode

--- lexicle.java
+++ lexicle.java
@@ -14,10 +14,10 @@ class Main {
                         "How to play:\n" +
                         "enter a starting word," +
                         "then repeatedly enter more words\n" +
-                        "until you guess the right word entirely\n" +
-                        "  *letter means letter is not in the word\n" +
-                        "  +letter means letter is in word, wrong spot\n" +
-                        "  letter (by itself) means letter is in word, right spot\n" +
+                        "until you guess the correct word entirely\n" +
+                        " >> *letter* means letter is not in the word\n" +
+                        " >> +letter+ means letter is in word, incorrect spot\n" +
+                        " >>  letter  (by itself) means letter is in word, correct spot\n" +
                         "good luck!\n ***** \n") ;
     
     //initializing options to play with
@@ -36,34 +36,63 @@ class Main {
       is_game_over = false ;
 
       // player action...
-      System.out.print("Would you like to:\n" + 
-                        "  a - add to your lexicon (dictionary)?\n" + 
+      System.out.print(".................................... \n" + 
+                        "Would you like to:\n" + 
+                        "  a - add to your lexicon (dictionary)?\n" +
+                        "  e - enable/disable random-length mode?\n" +
+                        "  s - see your dictionary?\n" +  
                         "  p - play the game?\n" +
-                        "  e - enable random-length mode?\n" +
                         "  q - quit?\n" + 
-                        "enter the number \n>>") ;
+                        "enter the mode's corresponding letter \n>>") ;
       String play = s.nextLine() ;
+      System.out.print(".................................... \n") ; 
 
       if (play.equals("q")) { // QUITTING
         is_play_over = false ;
       } 
-      else if (play.equals("e")) {
+      else if (play.equals("e")) { // RANDOM LENGTH MODE
         if (random_length_mode == false) {
           random_length_mode = true ;
         } else { // random_length_mode == true
-          //take in a required length of words (not just 5)?
           // remove all words from ans (dict) that are != 5 letters in length
+          for (int i = ans.size() - 1; i > 0; i--) {
+            if (ans.get(i).length() > 5) {
+              ans.remove(i) ;
+            }
+          }
+          random_length_mode = false ;
         }
+
+        String k ;
+        if (random_length_mode) 
+          k = "enabled" ;
+        else
+          k = "disabled" ;
+        System.out.println("you have " + k + " random length mode") ;
+      }
+      else if (play.equals("s")) { // SEE DICTIONARY
+        System.out.println("***** Your Dictionary *****") ;
+        for (String k : ans) {
+          System.out.println("  " + k) ;
+        }
+        System.out.println("***************************") ;
       }
       else if (play.equals("a")) { // ADD TO DICTIONARY
         //taking the words for the dictionary
-        System.out.println("enter a word \n>>") ;
+        System.out.println("enter a word") ;
         String f = "" ;
-        while (f.length() != 5) {
-          System.out.print("only 5-letter words allowed:\n>>") ;
+        if (!random_length_mode) {
+          while (f.length() != 5) {
+            System.out.print("only 5-letter words allowed:\n>>") ;
+            f = s.nextLine() ;
+          }
+        }
+        else {
+          System.out.print(">>");
           f = s.nextLine() ;
         }
         ans.add(f.toUpperCase()) ;
+        System.out.println("You have successfully added \"" + f + "\" to your dictionary") ;
       }
       else if (play.equals("p")) {// -- PLAY GAME
         //separation of key word and game
@@ -74,14 +103,16 @@ class Main {
         
         //setting the answer word to one from the dictionary
         String ans_word = ans.get((int)(Math.random()*ans.size())).toUpperCase() ;
+        int len = ans_word.length() ;
 
         while (!is_game_over) {
           String guesses = "" ;
 
           //taking a guess
-          System.out.println("enter your 5-letter guess") ;
-          while (guesses.length() != 5) {
-            System.out.print("only 5-letter words allowed:\n>>") ;
+          // len usually is = 5, unless you're in random length mode
+          System.out.println("enter your " + len + "-letter guess") ;
+          while (guesses.length() != len) {
+            System.out.print("only " + len + "-letter words allowed:\n>>") ;
             guesses = s.nextLine() ;
           }
           guess_count += 1 ;
@@ -89,19 +120,19 @@ class Main {
           //comparing
           String printing = "" ;
           boolean incorrect = false ;
-          for(int i = 0; i < ans_word.length(); i++) {
+          for(int i = 0; i < len; i++) {
             String ans_letter = ans_word.substring(i, i+1) ;
             String guess_letter = guesses.substring(i, i+1).toUpperCase() ;
             
             if(ans_letter.equals(guess_letter)) {
-              printing += guess_letter + "   " ;
+              printing += guess_letter + "    " ;
             }
             else if (ans_word.indexOf(guess_letter) != -1) {
-              printing += "+" + guess_letter + "  " ;
+              printing += "+" + guess_letter + "+  " ;
               incorrect = true ;
             }
             else {
-              printing += "*" + guess_letter + "  " ;
+              printing += "*" + guess_letter + "*  " ;
               incorrect = true ;
             }
           }
@@ -121,7 +152,8 @@ class Main {
           }
         }    
       } else { //bc sometimes people be dumb and don't follow instructions
-      
+        System.out.println("por favor, vuelves despues de aprender ingles") ;
+        System.out.println("or, if you know English, answer the prompt plz");
       }
   
     }
